21ce85c8da49d6ef7f43347021c19d7272dffc96
David Voskanyan <voskanyandavid001001@gmail.com>
2024-02-08 23:30:09 +0400

    branch init

--- serverSide/Authentication/index.ts
+++ serverSide/Authentication/index.ts
@@ -1,8 +1,9 @@
 import authenticationRouter from './router/AuthenitactionRouter';
+import cors from 'cors';
 import express from 'express';
 
 const app = express();
-
+app.use(cors());
 app.use('/auth', authenticationRouter);
 
 const PORT = process.env.PORT || 3000;
--- serverSide/Database/UserProvider/src/databaseConnection.ts
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('smart_team_database', 'root', 'pass', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-
-export default sequelize;
